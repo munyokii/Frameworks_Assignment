01c043b12ddab1597bf4d95dc181516b2d819a98
munyokii <munyoki912@gmail.com>
2025-09-17 17:04:48 +0300

    Data Cleaning and Preparation

--- cord19.py
+++ cord19.py
@@ -21,6 +21,23 @@ def explore_data(df):
     print("\nDescribing numerical columns:")
     print(df.describe())
 
+
+def clean_data(df):
+    """Data cleaning and preparation"""
+    # Converting publish time to datetime
+    df["publish_time"] = pd.to_datetime(df["publish_time"], errors="coerce")
+    df["year"] = df["publish_time"].dt.year
+
+    # Creating abstract word count as an example
+    df['abstract_word_count'] = df["abstract"].fillna("").apply(lambda x: len(x.split()))
+
+    #Removing rows with missing title or publish_time
+    df_clean = df.dropna(subset=["title", "publish_time"])
+
+    return df_clean
+
+
 if __name__ == "__main__":
     df = load_data()
     explore_data(df)
+    df_clean = clean_data(df)
